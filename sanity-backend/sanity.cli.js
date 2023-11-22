dc75e83ec9afcaf6f337a3cc802e5ff39040e108
rachealmuchiri <rachealloveo6@gmail.com>
2023-11-22 22:05:15 +0300

    new file

---
+++ sanity-backend/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '5qx6pv33',
+    dataset: 'production'
+  }
+})
---
+++ sanity-backend/schemas/index.js
@@ -0,0 +1,9 @@
+// Importing the schema types from about.js and brand.js
+import about from './about'
+import brand from './brand'
+
+// Creating an array of schema types
+const schemaTypes = [about, brand]
+
+// Exporting the array of schema types
+export {schemaTypes}
